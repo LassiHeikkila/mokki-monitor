e4e368eb544082d4bb7fc7281ee71858256b096c
Lassi Heikkila <lassi.heikkila@fara.fi>
2021-05-23 17:54:52 +0300

    Fix systemd scripts and make timesync.sh correctly use auto baud detection feature of SIM7000

--- timesync/timesync.sh
+++ timesync/timesync.sh
@@ -1,36 +1,31 @@
 #!/bin/bash
 
-function log() {
-	echo "$(date) $1"
-}
-
 function init() {
 	stty -F /dev/ttyS0 sane
 	stty -F /dev/ttyS0 115200 -parenb -parodd cs8 -echo
 
 	while true; do
-		2>/dev/null chat -t 1 -V '' 'AT' 'OK' > /dev/ttyS0 < /dev/ttyS0 && log "modem replied" && break
-		log "no response from modem"
-		echo "" > /dev/ttyS0
-		sleep 5
+		2>/dev/null chat -t 1 -V '' 'AT' 'OK' > /dev/ttyS0 < /dev/ttyS0 && echo "$(date) modem replied" && break
+		echo "$(date) no response from modem"
+                echo "" > /dev/ttyS0
+		sleep 1
 	done
 
-	# disable echo, otherwise it will mess up parsing +CCLK? response
 	2>/dev/null chat -t 1 -V '' 'ATE0' 'OK' > /dev/ttyS0 < /dev/ttyS0
 }
 
 init
 
 while true; do
-	log "asking time from modem"
+	echo "$(date) Asking time from modem"
 	2>/tmp/clk.file chat -t 3 -V '' 'AT+CCLK?' 'OK' > /dev/ttyS0 < /dev/ttyS0
 	timevar="$(cat /tmp/clk.file | grep +CCLK | cut -d' ' -f2)"
 	if [ -z "$timevar" ]; then
-		log "did not get result"
+		echo "$(date) Did not get result"
 		sleep 15
 		continue
 	fi
-	log "got result: $timevar"
+	echo "$(date) Got result: $timevar"
 	# looks like this, including quotation marks "21/04/16,19:36:21+12" # already in local time
 	year="20$(echo $timevar | cut -c2-3)"
 	month="$(echo $timevar | cut -c5-6)"
@@ -45,6 +40,6 @@ while true; do
 #	echo "minute is $min"
 #	echo "second is $second"
 
-	date -s "$year-$month-$day $hour:$min:$second" && log "time synchronized: \"$(date)\"" && exit
+	date -s "$year-$month-$day $hour:$min:$second" && echo "$(date) Time synchronized: \"$(date)\"" && exit
 	sleep 10
 done
